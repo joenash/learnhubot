3487dbde13ea5697a526507327956e38c306d773
Hugh Rawlinson <hughrawlinson@users.noreply.github.com>
2016-12-17 22:42:37 +0000

    Create spotiphy.js

---
+++ scripts/spotiphy.js
@@ -0,0 +1,17 @@
+// Spotiphy
+// Giphy but Spotify
+const SpotifyWebApi = require('spotify-web-api-node');
+const unauthenticatedSpotifyAPI = SpotifyWebApi();
+
+module.exports = (robot) => {
+  robot.hear(/spotiphy (.*)/i, (res) => {
+    res.send("HELLO BRIANO");
+    const query = res.match[0];
+    spotifyApi.searchTracks(query)
+      .then(function(data) {
+        res.send(data.tracks.items[0].external_urls.spotify);
+      }, function(err) {
+        res.send(":spotify: fail");
+      });
+  }
+}
