9d4662a655ed9ab75b8c961b842ff0f91363dd45
Hugh Rawlinson <hugh@spotify.com>
2016-12-18 12:55:13 +0000

    Spotiphy

--- scripts/spotiphy.js
+++ scripts/spotiphy.js
@@ -1,15 +1,15 @@
 // Spotiphy
 // Giphy but Spotify
 const SpotifyWebApi = require('spotify-web-api-node');
-const unauthenticatedSpotifyAPI = SpotifyWebApi();
+const unauthenticatedSpotifyAPI = new SpotifyWebApi();
 
-module.exports = (robot) => {
-  robot.hear(/spotiphy (.*)/i, (res) => {
-    const query = res.match[0];
+module.exports = function(robot){
+  robot.hear(/spotiphy (.*)/i, function(res){
+    const query = res.match[1];
     unauthenticatedSpotifyAPI.searchTracks(query)
-      .then(function(data) {
-        res.send(data.tracks.items[0].external_urls.spotify);
-      }, function(err) {
+      .then(function(data){
+        res.send(data.body.tracks.items[0].external_urls.spotify);
+      }, function(err){
         res.send(":spotify: fail");
       });
   });
